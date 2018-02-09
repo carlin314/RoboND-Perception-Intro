df02a733dbbc3b3b96775378a2827ef355e0ac4c
mkhuthir <mkhuthir@gmail.com>
2018-02-09 21:49:56 +0400

    voxel filter

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 # Import PCL module
 import pcl
 
@@ -5,24 +7,40 @@ import pcl
 cloud = pcl.load_XYZRGB('tabletop.pcd')
 
 
-# Voxel Grid filter
+# Voxel Grid Downsampling filter
+################################
+# Create a VoxelGrid filter object for our input point cloud
+vox = cloud.make_voxel_grid_filter()
 
+# Choose a voxel (also known as leaf) size
+# Note: this (1) means 1mx1mx1m is a poor choice of leaf size   
+# Experiment and find the appropriate size!
+LEAF_SIZE = 0.01   
+
+# Set the voxel (or leaf) size  
+vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+
+# Call the filter function to obtain the resultant downsampled point cloud
+cloud_filtered = vox.filter()
+filename = 'voxel_downsampled.pcd'
+pcl.save(cloud_filtered, filename)
 
 # PassThrough filter
-
+################################
 
 # RANSAC plane segmentation
-
+################################
 
 # Extract inliers
+################################
 
 # Save pcd for table
 # pcl.save(cloud, filename)
-
+################################
 
 # Extract outliers
-
+################################
 
 # Save pcd for tabletop objects
-
+################################
 
