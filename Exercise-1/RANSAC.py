2bb743a686a7f38ed2a98c8d1498129bb1c2730c
mkhuthir <mkhuthir@gmail.com>
2018-02-09 22:10:46 +0400

    pass through filter

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -27,6 +27,20 @@ pcl.save(cloud_filtered, filename)
 
 # PassThrough filter
 ################################
+# Create a PassThrough filter object.
+passthrough = cloud_filtered.make_passthrough_filter()
+
+# Assign axis and range to the passthrough filter object.
+filter_axis = 'z'
+passthrough.set_filter_field_name(filter_axis)
+axis_min = 0.6
+axis_max = 1.1
+passthrough.set_filter_limits(axis_min, axis_max)
+
+# Finally use the filter function to obtain the resultant point cloud. 
+cloud_filtered = passthrough.filter()
+filename = 'pass_through_filtered.pcd'
+pcl.save(cloud_filtered, filename)
 
 # RANSAC plane segmentation
 ################################
